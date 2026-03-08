93c44f4a4cc28d06293e357338358eea16f5c2d4
dunwood <zhouyahui@126.com>
2026-03-08 11:46:14 +0800

    Add files via upload

---
+++ scriptsrender-start.sh
@@ -0,0 +1,3 @@
+node openclaw.mjs config set gateway.controlUi.dangerouslyAllowHostHeaderOriginFallback true --strict-json
+
+exec node openclaw.mjs gateway --allow-unconfigured --bind lan --port 8080
