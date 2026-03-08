c19c3c52d4419e1eba560eec06d1c7dc37aa7126
dunwood <zhouyahui@126.com>
2026-03-08 12:10:26 +0800

    Delete scriptsrender-start.sh

--- scriptsrender-start.sh
+++
@@ -1,2 +0,0 @@
-node openclaw.mjs config set gateway.controlUi.dangerouslyAllowHostHeaderOriginFallback true --strict-json
-exec node openclaw.mjs gateway --allow-unconfigured --bind lan --port 8080
